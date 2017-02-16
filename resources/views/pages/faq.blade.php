daaeda8e26fae31b8f7c3190fec5b69d359a75a8
Robert McFarlane <rob.mcfarlane@hotmail.co.uk>
2017-02-16 23:33:09 +0000

    Add sections to FAQ page
    Added a group of divs into the FAQ page to provide the needed structure

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -3,12 +3,21 @@
 @section('title') Frequently Asked Questions @endsection
 
 @section('content')
-  <div class="container">
-    <div class="col-sm-6">
-      <div class="panel">
-        <h2>FAQ</h2>
-        <p>HELLO WELCOME TO FAQ </p>
-      </div>
+<div class="container">
+    <h1>FAQ</h1>
+    <div class="col-md-12">
+        <div class="panel">
+            <h2>Title 1</h2>
+            <p>This is the first FAQ entry</p>
+        </div>
+        <div class="panel">
+            <h2>Title 2</h2>
+            <p>This is the second FAQ entry</p>
+        </div>
+        <div class="panel">
+            <h2>Title 3</h2>
+            <p>This is the third FAQ entry</p>
+        </div>
     </div>
-  </div>
+</div>
 @endsection
