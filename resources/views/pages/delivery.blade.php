0dca09e00faa2e1b922a0eca7f73be12d759d0c3
Tom Boyle <Tom Boyle>
2017-02-16 14:34:29 +0000

    updated delivery page 2.0

--- resources/views/pages/delivery.blade.php
+++ resources/views/pages/delivery.blade.php
@@ -7,7 +7,7 @@
     <div class="col-sm-6">
       <div class="panel">
         <h2>Delivery</h2>
-        <p>Delivery comes in two options: Standard 3-5 day delivery, or Express 1-2 day Delivery.</p>
+        <p>Delivery comes in two options: Standard 3-5 day delivery, or Express 1-2 day Delivery. HAHA</p>
       </div>
     </div>
   </div>
