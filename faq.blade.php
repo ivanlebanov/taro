6eed41e5dfa05a69b464e0e0afb3e74356bf8e55
Tom Boyle <up679201@myport.ac.uk>
2017-02-16 14:18:39 +0000

    Dummy Test Commit
    Just a test commit to make sure I know what im doing

---
+++ faq.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title') Frequently Asked Questions @endsection
+
+@section('content')
+  <div class="container">
+    <div class="col-sm-6">
+      <div class="panel">
+        <h2>FAQ</h2>
+        <p>HELLO WELCOME TO FAQ </p>
+      </div>
+    </div>
+  </div>
+@endsection
