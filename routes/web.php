ed2eda3cd13b618a669e3c73f9f795795e9837fd
Ivan Lebanov <up719931@myport.ac.uk>
2017-02-20 20:30:37 +0000

    Change for validation

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::group(['middleware' => 'web'], function() {
   Route::get('/product/{id}/{name}', ['as' => 'products.single_product', 'uses' => 'ProductController@getSingleProductPage']);
   Route::get('/product/{id}', ['as' => 'product.getData', 'uses' => 'ProductController@getSingleProduct']);
   Route::post('/search',['as' => 'search.phrase', 'uses' => 'SearchController@search']);
+  Route::get('/search',['as' => 'search.phrase', 'uses' => 'SearchController@search']);
   Route::post('/search-results',['as' => 'search.results', 'uses' => 'SearchController@searchResults']);
 
   // Cart
