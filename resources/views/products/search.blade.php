20fdbd26c79595f6c57b9246925d994801983f12
Ivan Lebanov <up719931@myport.ac.uk>
2017-02-22 16:39:09 +0000

    Small enhancements

--- resources/views/products/search.blade.php
+++ resources/views/products/search.blade.php
@@ -4,17 +4,19 @@
 
 @section('content')
   @if(count($products) > 0)
-    @if($exactmatch)
-      @include('includes.list_products', [ 'products' => $products, 'title' => 'Your search for "' . $phrase . '" returned ' . count($products) . ' result(s):' ])
-    @else
-      @include('includes.list_products', [ 'products' => $products, 'title' => "We didn't find an exact match for  " . $phrase  . ". Products you may like:"])
-    @endif
+    <div class="container">
+      @if($exactmatch)
+        @include('includes.list_products', [ 'products' => $products, 'title' => 'Your search for "' . $phrase . '" returned ' . count($products) . ' result(s):' ])
+      @else
+        @include('includes.list_products', [ 'products' => $products, 'title' => "We didn't find an exact match for  " . $phrase  . ". Products you may like:"])
+      @endif
+    </div>
   @else
     <div class="container">
       <div class="col-md-7">
         <div class="panel">
           <h2>Your search for: {{$phrase}}</h2>
-          <p>Nothing found. Why not check out some <u><a href="../products/Laptops">Laptops?</a></u></p>
+          <p>Nothing found. Why not check out some <a href="{{ route('products.category', ['category' => 'Laptops' ])}}" class="simple_link">Laptops?</a></p>
         </div>
       </div>
     </div>
