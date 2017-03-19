79d6ea90fe66946ec8225ba29c605f5f31766cdb
Robert McFarlane <rob.mcfarlane@hotmail.co.uk>
2017-03-19 19:35:47 +0000

    Added makeAccount System test
    The completed test for creating an account on the system

---
+++ tests/helium/makeAccount.py
@@ -0,0 +1,28 @@
+from helium.api import *
+
+def main():
+    openSite("localhost:8000")
+    click("Login / Register")
+    click("Don't have an account? Register")
+    fillForm()
+    click("Register")
+    if (not (Text("The following errors have occurred:").exists())):
+        print("Test passed!")
+    else:
+        print("Test failed!")
+        print("Ensure that the database doesnt already contain the test data.")
+
+# loads the given url in chrome and maximizes the window
+def openSite(url):
+    start_chrome(url)
+    get_driver().maximize_window()
+
+def fillForm():
+    write("testaccount", into="Name")
+    write("test@email.com", into="Email")
+    write("alpha123STRING", into="Password")
+    write("alpha123STRING", into="Confirm password")
+
+# executes the main function when run, not imported.
+if __name__ == "__main__":
+    main()
