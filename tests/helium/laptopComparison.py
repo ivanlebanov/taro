a881dca056d0b4fbe49d9fb7d7202706ee0d4a83
Robert McFarlane <rob.mcfarlane@hotmail.co.uk>
2017-03-20 21:02:08 +0000

    Edit laptopComparison
    moved values to variables to simplify future changes to the test if
    necessary

--- tests/helium/laptopComparison.py
+++ tests/helium/laptopComparison.py
@@ -2,16 +2,19 @@ from helium.api import *
 from time import sleep
 from login import loginUser
 
+prod1 = "12-inch MacBook Pro"
+prod2 = "13-inch MacBook Pro"
+
 def main():
     openSite("localhost:8000")
     loginUser("test@email.com", "alpha123STRING")
     click("Laptops")
-    click(Button("compare", below="12-inch MacBook Pro"))
+    click(Button("compare", below=prod1))
     sleep(1)
-    click(Button("compare", below="13-inch MacBook Pro"))
+    click(Button("compare", below=prod2))
     hover("User account")
     click(Text("Compare", below="User account"))
-    if(Text("Comparing products").exists() and Text("12-inch MacBook Pro").exists() and Text("13-inch MacBook Pro").exists()):
+    if(Text("Comparing products").exists() and Text(prod1).exists() and Text(prod2).exists()):
         print("Test passed!")
     else:
         print("Test failed!")
